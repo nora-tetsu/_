7d24de99ceca59ffa0ba6babdee932d7e57ed21d
noratetsu <foamcrab@gmail.com>
2025-01-24 17:55:31 +0900

    追加: Deno/Gyazo

---
+++ Deno/Gyazo/client.ts
@@ -0,0 +1,62 @@
+// 2025/01/16 17:02
+import type { ImageData, UploadRequest, UploadResponse } from "./type.ts";
+
+export class GyazoClient {
+    token: string;
+    constructor(token: string) {
+        this.token = token;
+    }
+    list = async (page = 1, per_page = 20) => {
+        return await fetch('https://api.gyazo.com/api/images', {
+            method: 'POST',
+            body: JSON.stringify({
+                access_token: this.token,
+                page: page,
+                per_page: per_page,
+            }),
+        })
+            .then(response => response.json())
+            .then(json => json as ImageData[])
+    }
+    image = async (imageId: string) => {
+        return await fetch('https://api.gyazo.com/api/images/' + imageId, {
+            method: 'POST',
+            body: JSON.stringify({
+                access_token: this.token,
+            }),
+        })
+            .then(response => response.json())
+            .then(json => json as ImageData)
+    }
+    upload = async (req: UploadRequest | FormData) => {
+        const obj = Object.assign(req, { access_token: this.token });
+        return await fetch('https://upload.gyazo.com/api/upload', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+            },
+            body: JSON.stringify(obj),
+        })
+            .then(response => response.json())
+            .then(json => json as UploadResponse)
+    }
+    uploadWithURL = async (req: {
+        src: string,
+        url?: string,
+        title?: string,
+        description?: string,
+        date?: Date,
+    }) => {
+        const imageBlob = await fetch(req.src).then(response => response.blob());
+        const formData = new FormData();
+        formData.append('imagedata', imageBlob, req.title);
+        formData.append('access_token', this.token);
+        formData.append('access_policy', 'anyone');
+        formData.append('metadata_is_public', 'false');
+        if (req.url) formData.append('referer_url', req.url);
+        if (req.title) formData.append('title', req.title);
+        if (req.description) formData.append('desc', req.description);
+        if (req.date) formData.append('created_at', Math.floor(req.date.getTime() / 1000).toString());
+        return this.upload(formData)
+    }
+}
---
+++ Deno/Gyazo/type.ts
@@ -0,0 +1,48 @@
+export type ImageData = {
+    "image_id": string, // "8980c52421e452ac3355ca3e5cfe7a0c",
+    "permalink_url": string, // "http://gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c",
+    "thumb_url": string, // "https://i.gyazo.com/thumb/afaiefnaf.png",
+    "url": string,// "https://i.gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c.png",
+    "type": string, // "png",
+    "created_at": string,// "2014-05-21 14:23:10+0900",
+    "metadata": {
+        "app": string | null,
+        "title": string | null,
+        "url": string | null,
+        "desc": string | null
+    },
+    "ocr": {
+        "locale": string | null, // "en",
+        "description": string, // "Gyazo\n",
+    }
+}
+/*
+imagedata	binary	◯		multipart/form-dataのContent-Dispositionにおける`filename`を忘れずに付与してください。`filename`は必須です。
+access_policy	string anyone or only_me		anyone	画像の公開範囲を指定する文字列。 デフォルトは anyone で、リンクを知っている全員が閲覧可能です。 only_me を指定すると、アップロードしたユーザーのみが閲覧可能になります。
+metadata_is_public	string 'true' or 'false'			URLやタイトルなどのメタデータを公開するか否かの真偽値の文字列
+referer_url	string			キャプチャをしたサイトのURL
+app	string			キャプチャをしたアプリケーション名
+title	string			キャプチャをしたサイトのタイトル
+desc	string			任意のコメント
+created_at	float			画像の作られた日時（Unix time）
+collection_id	string			ユーザーが所有している/参加しているコレクションにのみ追加できます
+*/
+export type UploadRequest = {
+    imagedata: FormData,
+    access_policy?: 'anyone' | 'only_me',
+    metadata_is_public?: 'true' | 'false',
+    referer_url?: string,
+    app?: string,
+    title?: string,
+    desc?: string,
+    created_at?: number,
+    collection_id?: string,
+}
+
+export type UploadResponse = {
+    "image_id": string, // "8980c52421e452ac3355ca3e5cfe7a0c",
+    "permalink_url": string, // "http://gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c",
+    "thumb_url": string, // "https://i.gyazo.com/thumb/180/afaiefnaf.png",
+    "url": string, // "https://i.gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c.png",
+    "type": string, // "png"
+}
