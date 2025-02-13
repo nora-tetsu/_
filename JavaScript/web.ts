ea39a70fab615f7c27c2c4cb47f8a3c92f0d2d98
noratetsu <foamcrab@gmail.com>
2025-02-13 21:14:21 +0900

    更新: JavaScript/web.ts +getGyazoData

--- JavaScript/web.ts
+++ JavaScript/web.ts
@@ -38,6 +38,20 @@ export function expandShortenedURL(url: string) {
         });
 }
 
+type GyazoData = {
+    version: string,
+    type: string,
+    provider_name: string,
+    provider_url: string,
+    url: string, //http://i.gyazo.com/8c9d9c8ec14dec4631b6ec77d1c85450_1.png,
+    width: number,
+    height: number,
+}
+export function getGyazoData(gyazoUrl: string) {
+    return fetch(`https://api.gyazo.com/api/oembed?url=${gyazoUrl}`)
+        .then(res => res.json()) as Promise<GyazoData>
+}
+
 type YouTubeData = {
     kind: string;
     etag: string;
@@ -73,7 +87,7 @@ type YouTubeData = {
             definition: string;
             caption: string;
             licensedContent: boolean;
-            contentRating: {};
+            contentRating: unknown;
             projection: string;
         },
         status: {
