3e35c84c7237d059e2350344486fecf0790227a1
noratetsu <foamcrab@gmail.com>
2025-03-31 17:01:43 +0900

    新規: Deno/file.ts getFilesData サブディレクトリを含めてファイルデータを取得する

---
+++ Deno/file.ts
@@ -0,0 +1,22 @@
+import { createPath } from "Module/util.ts";
+
+export function getFilesData<T>(rootPath: string, fileDataGetter: (filename: string, text: string, dirPath: string, stat: Partial<Deno.FileInfo>) => T|Promise<T>) {
+    const result: T[] = [];
+    const roop = async (path: string) => {
+        const files = Deno.readDirSync(path);
+        for (const file of files) {
+            const dir = createPath(path, file.name);
+            if (file.isFile) {
+                const text = Deno.readTextFileSync(dir);
+                const stat = Deno.statSync(dir);
+                const fileData = await fileDataGetter(file.name, text, path, stat);
+                result.push(fileData);
+            } else if (file.isDirectory) {
+                if (file.name.startsWith(".")) continue;
+                roop(dir);
+            }
+        }
+    }
+    roop(rootPath);
+    return result;
+}
