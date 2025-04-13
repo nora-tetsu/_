60a22c2f002e725e806a1ff38335daceffdfd6d2
noratetsu <foamcrab@gmail.com>
2025-04-13 21:33:55 +0900

    改良: JSX/components/ui/Button.tsx FaButtonの引数の型を改良

--- JSX/components/ui/Button.tsx
+++ JSX/components/ui/Button.tsx
@@ -1,57 +1,58 @@
-import {JSX, type Signal} from "../deps.ts";
+import { JSX, type Signal } from "../deps.ts";
 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
-    return (
-      <button
-        {...props}
-      />
-    );
-  }
-  
-  export function Button2(props: JSX.HTMLAttributes<HTMLButtonElement>) {
-    return (
-      <button
-        {...props}
-        class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-      />
-    );
-  }
-  
-  export function FaButton(
-    { fontawesome, onClick, title }: {
-      fontawesome: string;
-      onClick: (e: MouseEvent) => void;
-      title: string;
-    },
-  ) {
-    return (
-      <i
-        class={fontawesome + " button"}
-        onClick={onClick}
-        title={title}
-      />
-    );
-  }
-  
-  function toggleSignalBool(signal: Signal<boolean>) {
-    const current = signal.value;
-    signal.value = !current;
-  }
-  
-  export function FaToggleButton(
-    { fontawesome, signal, title }: {
-      fontawesome: string;
-      signal: Signal<boolean>;
-      title: string;
-    },
-  ) {
-    return (
-      <i
-        class={fontawesome + " button"}
-        data-on={String(signal.value)}
-        onClick={() => toggleSignalBool(signal)}
-        title={title}
-      />
-    );
-  }
-  
+  return (
+    <button
+      {...props}
+    />
+  );
+}
+
+export function Button2(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
+    />
+  );
+}
+
+type FaButtonProps = {
+  fontawesome: string;
+  onClick: (e: MouseEvent) => void;
+  title: string;
+} & JSX.HTMLAttributes<HTMLElement>;
+
+type FaToggleButtonProps = {
+  fontawesome: string;
+  signal: Signal<boolean>;
+  title: string;
+} & JSX.HTMLAttributes<HTMLElement>;
+
+export function FaButton({ fontawesome, onClick, title, ...rest }: FaButtonProps) {
+  return (
+    <i
+      {...rest}
+      class={fontawesome + " button"}
+      onClick={onClick}
+      title={title}
+    />
+  );
+}
+
+function toggleSignalBool(signal: Signal<boolean>) {
+  const current = signal.value;
+  signal.value = !current;
+}
+
+export function FaToggleButton({ fontawesome, signal, title, ...rest }: FaToggleButtonProps) {
+  return (
+    <i
+      {...rest}
+      class={fontawesome + " button"}
+      data-on={String(signal.value)}
+      onClick={() => toggleSignalBool(signal)}
+      title={title}
+    />
+  );
+}
