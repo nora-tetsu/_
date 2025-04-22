a714841417711c15f4f69ed60e8adbf9b315b777
noratetsu <foamcrab@gmail.com>
2025-04-22 21:46:10 +0900

    整理: JavaScript/Notion/props.ts namespaceに収めた

--- JavaScript/Notion/props.ts
+++ JavaScript/Notion/props.ts
@@ -1,10 +1,5 @@
 // deno-lint-ignore-file no-namespace
 
-const DATABASE_ID = {
-    hololive_events: "1b9c674359f180b18a3af577200df8dc",
-    hololive_diary: "1b9c674359f18012ab65f1370bf7e276",
-}
-
 function generateDatabaseObject(database_id: string) {
     return {
         "type": "database_id",
@@ -12,153 +7,146 @@ function generateDatabaseObject(database_id: string) {
     }
 }
 
-export const hololiveEventsParent = generateDatabaseObject(DATABASE_ID.hololive_events);
-export const hololiveDiaryParent = generateDatabaseObject(DATABASE_ID.hololive_diary);
-
-export type HololiveEventsProps = {
-    内容: string,
-    タイトル: string,
-    日付: { start: string, end?: string },
-    ホロメン: string,
-    チャンネル: string,
-    URL: string,
-    種類: string[],
-    備考: string,
-}
-export function createEventsProperties(prop: HololiveEventsProps) {
-    let date;
-    if (!prop.日付.start) {
-        date = null;
-    } else {
-        date = {
-            "start": prop.日付.start,
-            "end": prop.日付.end || null,
-            "time_zone": null
-        }
-    }
-    return {
-        "内容": {
-            "title": [
-                {
-                    "text": {
-                        "content": prop.内容
-                    }
-                }
-            ]
-        },
-        "タイトル": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": prop.タイトル
-                    }
-                }
-            ]
-        },
-        "日付": {
-            "date": date
-        },
-        "ホロメン": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": prop.ホロメン
-                    }
-                }
-            ]
-        },
-        "チャンネル": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": prop.チャンネル
-                    }
-                }
-            ]
-        },
-        "URL": {
-            "url": prop.URL || null
-        },
-        "備考": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": prop.備考
-                    }
-                }
-            ]
-        },
-        "種類": {
-            "multi_select": prop.種類.map(type => {
-                return { name: type };
-            })
-        },
-    }
-}
-
-export type HololiveDiaryProps = {
-    内容: string,
-    日付: { start: string, end?: string },
-    ホロメン: string,
-    種類: string[],
-    対象: string[],
-}
-export function createDiaryProperties(prop: HololiveDiaryProps) {
-    let date;
-    if (!prop.日付.start) {
-        date = null;
-    } else {
-        date = {
-            "start": prop.日付.start,
-            "end": prop.日付.end || null,
-            "time_zone": null
-        }
-    }
-    return {
-        "内容": {
-            "title": [
-                {
-                    "text": {
-                        "content": prop.内容
-                    }
-                }
-            ]
-        },
-        "日付": {
-            "date": date
-        },
-        "ホロメン": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": prop.ホロメン
-                    }
-                }
-            ]
-        },
-        "種類": {
-            "multi_select": prop.種類.map(type => {
-                return { name: type };
-            })
-        },
-        "対象": {
-            "relation": prop.対象.map(target => {
-                return { id: target };
-            })
-        },
-    }
-}
-
 export namespace HololiveEvents {
-    export type Props = HololiveEventsProps;
-    export const parent = hololiveEventsParent;
-    export const createProperties = createEventsProperties;
+    export type Props = {
+        内容: string,
+        タイトル: string,
+        日付: { start: string, end?: string },
+        ホロメン: string,
+        チャンネル: string,
+        URL: string,
+        種類: string[],
+        備考: string,
+    };
+    export const DATABASE_ID = "1b9c674359f180b18a3af577200df8dc";
+    export const parent = generateDatabaseObject(HololiveEvents.DATABASE_ID);
+    export const createProperties = (prop: HololiveEvents.Props) => {
+        let date;
+        if (!prop.日付.start) {
+            date = null;
+        } else {
+            date = {
+                "start": prop.日付.start,
+                "end": prop.日付.end || null,
+                "time_zone": null
+            }
+        }
+        return {
+            "内容": {
+                "title": [
+                    {
+                        "text": {
+                            "content": prop.内容
+                        }
+                    }
+                ]
+            },
+            "タイトル": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": prop.タイトル
+                        }
+                    }
+                ]
+            },
+            "日付": {
+                "date": date
+            },
+            "ホロメン": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": prop.ホロメン
+                        }
+                    }
+                ]
+            },
+            "チャンネル": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": prop.チャンネル
+                        }
+                    }
+                ]
+            },
+            "URL": {
+                "url": prop.URL || null
+            },
+            "備考": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": prop.備考
+                        }
+                    }
+                ]
+            },
+            "種類": {
+                "multi_select": prop.種類.map(type => {
+                    return { name: type };
+                })
+            },
+        }
+    };
 }
 
 export namespace HololiveDiary {
-    export type Props = HololiveDiaryProps;
-    export const parent = hololiveDiaryParent;
-    export const createProperties = createDiaryProperties;
+    export type Props = {
+        内容: string,
+        日付: { start: string, end?: string },
+        ホロメン: string,
+        種類: string[],
+        対象: string[],
+    };
+    export const DATABASE_ID = "1b9c674359f18012ab65f1370bf7e276";
+    export const parent = generateDatabaseObject(HololiveDiary.DATABASE_ID);
+    export const createProperties = (prop: HololiveDiary.Props) => {
+        let date;
+        if (!prop.日付.start) {
+            date = null;
+        } else {
+            date = {
+                "start": prop.日付.start,
+                "end": prop.日付.end || null,
+                "time_zone": null
+            }
+        }
+        return {
+            "内容": {
+                "title": [
+                    {
+                        "text": {
+                            "content": prop.内容
+                        }
+                    }
+                ]
+            },
+            "日付": {
+                "date": date
+            },
+            "ホロメン": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": prop.ホロメン
+                        }
+                    }
+                ]
+            },
+            "種類": {
+                "multi_select": prop.種類.map(type => {
+                    return { name: type };
+                })
+            },
+            "対象": {
+                "relation": prop.対象.map(target => {
+                    return { id: target };
+                })
+            },
+        }
+    };
 }
 
 export namespace FanArts {
@@ -168,7 +156,8 @@ export namespace FanArts {
         Genre: string[];
         Date: { start: string, end?: string },
     }
-    export const parent = generateDatabaseObject("1dc3c17be12980b18b34d27fd6dffcde");
+    export const DATABASE_ID = "1dc3c17be12980b18b34d27fd6dffcde";
+    export const parent = generateDatabaseObject(FanArts.DATABASE_ID);
     export const createProperties = (prop: FanArts.Props) => {
         let date;
         if (!prop.Date.start) {
