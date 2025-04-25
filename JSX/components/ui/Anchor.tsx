43c40095ca2f23ad5200ddd9583711963db1faf7
noratetsu <foamcrab@gmail.com>
2025-04-25 11:22:57 +0900

    更新: JSX/components/ui/Anchor.tsx 特定のサイトのfavicon選択を定数で定義

--- JSX/components/ui/Anchor.tsx
+++ JSX/components/ui/Anchor.tsx
@@ -1,4 +1,5 @@
 import { JSX } from "../deps.ts";
+import { FAVICON_DATA } from "../../static/constants.ts";
 
 export function ExAnchor(props: JSX.HTMLAttributes<HTMLAnchorElement>) {
     return (
@@ -8,8 +9,18 @@ export function ExAnchor(props: JSX.HTMLAttributes<HTMLAnchorElement>) {
     );
 }
 
+const url2FaviconSrc = (url: string) => "https://www.google.com/s2/favicons?domain=" + url;
+const getFaviconSrc = (url: string) => {
+    const find = FAVICON_DATA.find(({ starts }) => url.startsWith(starts));
+    if (find) {
+        return find.isIcon ? find.src : url2FaviconSrc(find.src);
+    } else {
+        return url2FaviconSrc(url);
+    }
+}
+
 export function ExternalLinkWithIcon({ url, title, imgsrc, tooltip }: { url: string; title: string, imgsrc?: string, tooltip?: string }) {
-    const src = imgsrc || "	https://www.google.com/s2/favicons?domain=" + url;
+    const src = imgsrc || getFaviconSrc(url);
     return (
         <>
             <img src={src} width={12} height={12} alt="icon" />
---
+++ JSX/static/constants.ts
@@ -0,0 +1,7 @@
+
+export const FAVICON_DATA: { starts: string, src: string, isIcon?: boolean }[] = [
+    { starts: "https://noratetsu.blogspot.com/", src: "https://www.blogger.com/" },
+    { starts: "https://scrapbox.io/", src: "https://scrapbox.io/" },
+    { starts: "https://docs.google.com/forms/", src: "https://www.google.com/intl/ja/forms/about/" },
+    { starts: "https://noratetsuhouse.pages.dev/rss.xml", src: "https://gyazo.com/29551b2d37b3d6383a6735db611617c9/raw", isIcon: true },
+]
