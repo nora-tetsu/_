36cde290cf27337aa1552fc962f798f4fb51efe1
noratetsu <foamcrab@gmail.com>
2025-04-25 13:05:02 +0900

    追加: JSX/static/constants.ts FAVICON_DATA

--- JSX/static/constants.ts
+++ JSX/static/constants.ts
@@ -2,6 +2,8 @@
 export const FAVICON_DATA: { starts: string, src: string, isIcon?: boolean }[] = [
     { starts: "https://noratetsu.blogspot.com/", src: "https://www.blogger.com/" },
     { starts: "https://scrapbox.io/", src: "https://scrapbox.io/" },
+    { starts: "https://noratetsu.notion.site/", src: "https://www.notion.com/" },
+    { starts: "https://twitter.com/", src: "https://x.com/" },
     { starts: "https://docs.google.com/forms/", src: "https://www.google.com/intl/ja/forms/about/" },
     { starts: "https://noratetsuhouse.pages.dev/rss.xml", src: "https://gyazo.com/29551b2d37b3d6383a6735db611617c9/raw", isIcon: true },
 ]
